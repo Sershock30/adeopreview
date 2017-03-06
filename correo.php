8614c5808d203cab8a332969d101edab2bc38690
Sergio Serrano <sergioalejserrano@gmail.com>
2017-03-06 11:57:33 -0600

    Contacto funcional

--- correo.php
+++ correo.php
@@ -1,22 +1,67 @@
 <?php
-$nombre = $_POST["nombre"];
-$company = $_POST["company"];
-$email = $_POST["email"];
-$area = $_POST["areas"];
-$mensaje = $_POST["mensaje"];
-$para = "info@adeosoluciones.com";
-$titulo = "Contacto Página Web";
-$header = "From:" . $email;
-$msjCorreo = "Nombre: $nombre\n Compañia: $company \n E-Mail: $email \n Area: $area\n Mensaje:\n $mensaje";
+   function form_mail($sPara, $sAsunto, $sTexto, $sDe){
 
-if ($_POST["submit"]) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
-echo '<script  language="javascript">
-alert("Mensaje enviado, muchas gracias.");
-window.location.href = "http://adeosoluciones.com";
-</script>';
-} else {
-echo 'Falló el envio';
-}
-}
+      $bHayFicheros = 0;
+      $sCabeceraTexto = "";
+      $sAdjuntos = "";
+
+      if ($sDe)$sCabeceras = "From:".$sDe."\n"; else $sCabeceras = "";
+      $sCabeceras .= "MIME-version: 1.0\n charset=UTF-8' \n";
+
+      foreach ($_POST as $sNombre => $sValor)
+             $sTexto = $sTexto."\n".$sNombre." = ".$sValor;
+
+      foreach ($_FILES as $vAdjunto){
+
+             if ($bHayFicheros == 0){
+
+                $bHayFicheros = 1;
+
+                $sCabeceras .= "Content-type: multipart/mixed;";
+               $sCabeceras .= "boundary=\"--_Separador-de-mensajes_--\"\n";
+
+               $sCabeceraTexto = "----_Separador-de-mensajes_--\n";
+               $sCabeceraTexto .= "Content-type: text/plain;charset=utf-8\n";
+               $sCabeceraTexto .= "Content-transfer-encoding: 7BIT\n";
+
+               $sTexto = $sCabeceraTexto.$sTexto;
+
+             }
+
+             if ($vAdjunto["size"] > 0){
+
+                $sAdjuntos .= "\n\n----_Separador-de-mensajes_--\n";
+                $sAdjuntos .= "Content-type: ".$vAdjunto["type"].";name=\"".$vAdjunto["name"]."\"\n";;
+               $sAdjuntos .= "Content-Transfer-Encoding: BASE64\n";
+               $sAdjuntos .= "Content-disposition: attachment;filename=\"".$vAdjunto["name"]."\"\n\n";
+
+                $oFichero = fopen($vAdjunto["tmp_name"], 'r');
+               $sContenido = fread($oFichero, filesize($vAdjunto["tmp_name"]));
+               $sAdjuntos .= chunk_split(base64_encode($sContenido));
+               fclose($oFichero);
+             }
+
+      }
+
+      if ($bHayFicheros)
+         $sTexto .= $sAdjuntos."\n\n----_Separador-de-mensajes_----\n";
+
+      return(mail($sPara, $sAsunto, $sTexto, $sCabeceras));
+   }
+
+   //Ejemplo de como usar:
+   if (form_mail("info@adeosoluciones.com",
+              "Contacto de Página Web",
+              "Los datos introducidos en el formulario son:\n\n",
+              Contacto
+             )
+      )
+      echo '<script  language="javascript">
+            alert("Mensaje enviado, muchas gracias.");
+            window.location.href = "http://adeosoluciones.com";
+            </script>';
+    // header("Location: index.html"); /* Redirect browser */
+    exit();
+      // echo "Su formulario ha sido enviado con exito";
+
 ?>
